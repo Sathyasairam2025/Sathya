3ca83712f5da72d74d519d2efcc151038804dd3d
Sathyasairam2025 <sairamec049@gmail.com>
2024-07-14 10:56:07 +0530

    java day23.java

---
+++ java day23.java
@@ -0,0 +1,20 @@
+ NokiaSeries
+//public class NokiaSeries {
+
+}//
+
+ SamsungPhone
+//public @interface SamsungPhone {
+
+}//
+ Smartphone
+   
+//public @interface Smartphone {
+//	String os() default "Android";
+//	int version();
+//}
+
+ Waterproof
+//public @interface Waterproof {
+//String color();
+//}
